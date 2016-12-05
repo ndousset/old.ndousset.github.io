3b5cd2cedf55f2ccd87c9747d6b7be77217bf638
ndousset <ndousset@hotmail.com>
2016-12-05 13:04:48 +0000

    add webdev notes

--- stickynav.js
+++ stickynav.js
@@ -1,6 +1,6 @@
 $(function(){
   $(window).scroll(function() {
-    if ($(this).scrollTop()>= 230) {
+    if ($(this).scrollTop()>= 250) {
        $('nav.main-nav').addClass('stickytop');
     }
     else {
